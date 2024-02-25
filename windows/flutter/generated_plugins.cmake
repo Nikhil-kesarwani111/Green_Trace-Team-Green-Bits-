d1356fed7e24b37274cc80ff4b2554c328cb00a8
Nikhil-kesarwani111 <nikhilkesarwani354@gmail.com>
2024-02-25 08:08:59 +0530

    My final project

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,6 +3,12 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  file_selector_windows
+  firebase_auth
+  firebase_core
+  firebase_storage
+  share_plus
+  url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
